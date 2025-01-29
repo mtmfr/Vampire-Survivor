30f4540274574acc9715b58c3e8d10e225cd6b2c
mtmfr <119870806+mtmfr@users.noreply.github.com>
2025-01-29 09:11:22 +0100

    Character
    -Change to antonio
    - Imelda added

--- Assets/ScriptableObject/Script/Character/Antonio.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Antonio", menuName = "Scriptable Objects/Character/Antonio")]
-public class Antonio : SO_Character
-{
-    
-}
--- Assets/ScriptableObject/Script/Character/SO_Character.cs
+++ Assets/ScriptableObject/Script/Character/SO_Character.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 
-public abstract class SO_Character : ScriptableObject
+[CreateAssetMenu(fileName = "PlayerCharacter", menuName = "Scriptable Objects/Character")]
+public class SO_Character : ScriptableObject
 {
+    [field: SerializeField] public string characterName { get; protected set; }
     [field: SerializeField] public Sprite CharacterSprite { get; protected set; }
     [field: SerializeField] public RuntimeAnimatorController CharacterAnim { get; protected set; }
     [field: SerializeField] public Weapon StartingWeapon { get; protected set; }
+
+    private void OnValidate()
+    {
+        #if UNITY_EDITOR
+        characterName = name;
+        UnityEditor.EditorUtility.SetDirty(this);
+        #endif
+
+    }
 }
